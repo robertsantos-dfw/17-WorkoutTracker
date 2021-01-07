b372b4e570308233a8a911ec4478c99c42ae4faf
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-07 16:10:21 -0600

    updated mongodb connection and routes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
+
+const app = express();
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
-
-const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,17 +12,18 @@ app.use(express.static("public"));
 
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb+srv://robertsantos_dfw:Hello123@cluster0.9ntmj.mongodb.net/workout_tracker?retryWrites=true&w=majority', {
+    process.env.MONGODB_URI || 'mongodb://localhost/workout', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
         useFindAndModify: false
     }
+
 );
 
 // routes
-app.use(require("api.js"));
-app.use(require("view.js"));
+app.use(require("./routes/api.js"));
+app.use(require("./routes/workout.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
