704ab66fbe0cba0335439c93406b0ab5310637db
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-08 08:37:53 -0600

    Updated Routes

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
 
 const app = express();
 app.use(logger("dev"));
@@ -14,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
+app.set('view engine', 'html');
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout', {
@@ -27,7 +30,19 @@ mongoose.connect(
 );
 
 // routes
-app.use(require("./public/api.js"));
+require("./routes/api-routes.js")(app);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
